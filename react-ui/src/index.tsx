9b376156883c61810316023b5e9ebf89ff5e4971
FrancoGargaglioni <FrancoGargaglioni@gmail.com>
2025-04-03 19:19:06 -0300

    Setup de front

--- react-ui/src/index.tsx
+++ react-ui/src/index.tsx
@@ -1,16 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as  Router, Routes, Route } from "react-router";
+
+import Home from './pages/Home';
+import App from './App';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
+
   <React.StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
   </React.StrictMode>
+
 );
 
 // If you want to start measuring performance in your app, pass a function
