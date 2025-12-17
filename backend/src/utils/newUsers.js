3f42d705fd28790cae69df9e424f1ffd5b4286bb
FrancoGargaglioni <FrancoGargaglioni@gmail.com>
2025-12-17 17:56:22 -0300

    Nuevo estilo, horacio icono y nuevos usuarios y contraseñas

---
+++ backend/src/utils/newUsers.js
@@ -0,0 +1,499 @@
+
+const rawList = `
+Mathias Rivero	mrivero
+Yonathan Pla	ypla
+Palmira Bengochea	pbengochea
+Sezen Asenova	sasenova
+José Luis Martínez	jmartínez
+Iván Ramirez	iramirez
+Ximena Páez	xpáez
+Estefanía Rosso	erosso
+Federico Abdo	fabdo
+Santiago de los santos	ssantos
+Analia Scapino	ascapino
+Valentina Palladino	vpalladino
+Alejo Gestal	agestal
+Marcos Brites	mbrites
+Florencia Gonnet	fgonnet
+Lucia Olivera	lolivera
+Macarena Fernández	mfernández
+Florencia Ferreira	fferreira
+Gastón Aboal	gaboal
+Fabricio Fernández	ffernández
+Mario Nicolini	mnicolini
+Abril Perdomo	aperdomo
+Mariela Chocho	mchocho
+Lucio Vera	lvera
+Mauro Yiansens	myiansens
+Valeria Bernasconi	vbernasconi
+Silvina Angüilla	sangüilla
+Juan Gabriel Garcia	jgarcia
+Marcos Zinnato	mzinnato
+Gonzalo Esposto	gesposto
+Mariana Neves	mneves
+Matilde acosta	macosta
+Virginia Ormazabal	vormazabal
+Emanuel Sosa	esosa
+Cristian Díaz	cdíaz
+Bruno Silvestri	bsilvestri
+Gonzalo Paredes	gparedes
+Matias Ortiz	mortiz
+German Gimenez	ggimenez
+Damian Clavell	dclavell
+Laura Conti	lconti
+Valentina	vvalentina
+Carmela Machado	cmachado
+Selva Gelso	sgelso
+Jessica Rosano	jrosano
+Valentina Ramirez	vramirez
+Joaquín Pereira	jpereira
+Cris Castro	ccastro
+Ismael González	igonzález
+Santiago Miguel	smiguel
+Germán Arén	garén
+Noha Camaño	ncamaño
+Laura Cabrera	lcabrera
+Julieta Sosa	jsosa
+Ana Duce	aduce
+Lucia Frizzi	lfrizzi
+Stefanie Vázquez	svázquez
+Nicolas Bichinque	nbichinque
+Esmeralda Pieri	epieri
+Antonella Díaz	adíaz
+Chiara inzaurralde	cinzaurralde
+Nicolás Viera	nviera
+Joaquina Goicoechea	jgoicoechea
+Romina Dominzain	rdominzain
+diego rosati	drosati
+Roman Ferreira	rferreira
+Rodrigo Silva	rsilva
+Cristian Marrero	cmarrero
+Moriana Gimenez	mgimenez
+Diandra de Aguiar	daguiar
+Alicia Martínez	amartínez
+Juan David	jdavid
+Carolina Antúnez	cantúnez
+Sofia Dri	sdri
+Cecilia Buonomo	cbuonomo
+Sofía ferreira	sferreira
+Victoria Goyret	vgoyret
+Michael Pla	mpla
+Andrea Carbonell	acarbonell
+José Abreu	jabreu
+Helian Gómez	hgómez
+Joshua Cardozo	jcardozo
+Miriam Gonzalez	mgonzalez
+Mauro Navarrine	mnavarrine
+Matilde Leites	mleites
+Pablo Pereyra	ppereyra
+Ivana Toribio	itoribio
+Allison Leoni	aleoni
+Josefa Flores	jflores
+Rocío Lión	rlión
+Natalia Rodríguez	nrodríguez
+Marinele De paula	mpaula
+Germán Olazabal	golazabal
+Martina Sosa	msosa
+Sofía Draper	sdraper
+Virginia Pérez	vpérez
+Matías Méndez	mméndez
+Nicolás Suárez	nsuárez
+Ana Bentancor	abentancor
+Agustin Hu	ahu
+Clara Gómez	cgómez
+NATALIA VARELA	nvarela
+Marcel Barceló	mbarceló
+Federico Gomez	fgomez
+Adriana Longo	alongo
+Francisco Pieri	fpieri
+Carina Pacheco	cpacheco
+Mariana pouso	mpouso
+Milena Cantero	mcantero
+Luciana Britos	lbritos
+Victoria sosa	vsosa
+Daniela Malleville	dmalleville
+Cecilia Barate	cbarate
+Sabrina Elgart	selgart
+Nico Tolstoy	ntolstoy
+Sheila Crida	scrida
+Bruno Dho	bdho
+Juana de los santos	jsantos
+Regignaldo Gonzalez	rgonzalez
+Claudia Rodríguez	crodríguez
+Karina Casavieja	kcasavieja
+Daniela González	dgonzález
+Andrea Dominzain	adominzain
+Augusto Deganello	adeganello
+Joaquín Sanabria	jsanabria
+Natalia Chiesa	nchiesa
+Camila Riesgo	criesgo
+Federico Guridi	fguridi
+Andres Delfino	adelfino
+Pablo ferragut	pferragut
+Priscilla Suarez	psuarez
+Dana Silva	dsilva
+Mercedes Méndez	mméndez
+Fabian Leoni	fleoni
+Andres Saavedra	asaavedra
+Mauricio Chirino	mchirino
+Gonzalo Vallarino	gvallarino
+Ramiro Hernandez	rhernandez
+Sandra Moreno	smoreno
+Romina Sosa	rsosa
+Cecilia Ogara	cogara
+Juan Pereyra	jpereyra
+Vanessa Astapenco	vastapenco
+Federico Mello	fmello
+Salomé Campot	scampot
+Sofía Caballero	scaballero
+Ruddy Moncada	rmoncada
+Ignacio Marquez	imarquez
+Antonio Manzi	amanzi
+Higinio Duarte	hduarte
+Natalia García	ngarcía
+Catalina Barceló	cbarceló
+Tomás Garula	tgarula
+Silvina Vilches	svilches
+Lia Lloveras	llloveras
+Joselin Damian	jdamian
+Marcelo Lozano	mlozano
+Luis Fernández	lfernández
+Guillermo Suárez	gsuárez
+Jorge Fuentes	jfuentes
+Federico Abdo	fabdo
+John Pirez	jpirez
+Marelen Cabrera	mcabrera
+Lucas guedes	lguedes
+Braian Texeira	btexeira
+Lucia Fornaroli	lfornaroli
+Luis Abdo	labdo
+Daniel Riesgo	driesgo
+Patricia Riesgo	priesgo
+Lois Ferreira	lferreira
+Amparo Bengochea	abengochea
+Elsa Romero	eromero
+María Sosa	msosa
+Nicolas Navascues	nnavascues
+Claudia Reyes	creyes
+josefina barceló	jbarceló
+Federico Lemos	flemos
+Martin Saavedra	msaavedra
+Andrés Kent	akent
+Pablo Pinazzo	ppinazzo
+Gonzalo Marmissolle	gmarmissolle
+Antonella Sosa	asosa
+Carolina Urioste	curioste
+Maximiliano Nogueira	mnogueira
+Emilse Chá	echá
+Lucas Fuente	lfuente
+Valentín Pérez	vpérez
+Francisco González	fgonzález
+Magdalena Risvegliato	mrisvegliato
+Santiago Sánchez	ssánchez
+Diego Fuentes	dfuentes
+Facundo Andrioli	fandrioli
+Martín Riesgo	mriesgo
+Fernando Mangold	fmangold
+Victor Estigarribia	vestigarribia
+Jessica Paez	jpaez
+Clara Gonzalez	cgonzalez
+Lorena Guardia	lguardia
+Marco Bonino	mbonino
+Federico Prieto	fprieto
+Nahuel Granero	ngranero
+Nicolás Zinoveev	nzinoveev
+Ricardo pla	rpla
+Emilia Diaz	ediaz
+Paula Galíndez	pgalíndez
+Lucía Prieto	lprieto
+Paula Pouso	ppouso
+Genaro Loprete	gloprete
+Andrea Olariaga	aolariaga
+Luis Chiappara	lchiappara
+Agustín ibero	aibero
+Gabriel Ducer	gducer
+Hebenor Bermúdez	hbermúdez
+Franco Alvarez	falvarez
+Óscar Gutiérrez	ógutiérrez
+Lorena Fleitas	lfleitas
+Franco Gargaglioni	fgargaglioni
+Joaquin Silva	jsilva
+Fernando Martínez	fmartínez
+Alejandro González	agonzález
+Guillermo González	ggonzález
+Verónica Pinazzo	vpinazzo
+Nahuel Quiroga	nquiroga
+Rodrigo Correa	rcorrea
+Emanuel Berón	eberón
+Emanuel Barrera	ebarrera
+Fabio Gatti	fgatti
+Rodrigo Sánchez	rsánchez
+Rodrigo Marote	rmarote
+Pablo Hernández	phernández
+Nicolás Revello	nrevello
+Máximo Saavedra	msaavedra
+Marta Borelli	mborelli
+Marcos Zinatto	mzinatto
+Jonathan Sjostrand	jsjostrand
+Gustavo Ricci	gricci
+Gissel Martins	gmartins
+Felipe Hein	fhein
+Enzo de Nicola	enicola
+Emil Chichet	echichet
+Diego Pilosof	dpilosof
+Darío García	dgarcía
+Avril Pardo	apardo
+Agustín Rodríguez	arodríguez
+Agustín Da Silveira	asilveira
+Fernando Abdo	fabdo
+`;
+module.exports = rawList.trim();
+
+// "[
+// 'Mathias Rivero	mrivero',
+// 'Yonathan Pla	ypla',
+// 'Palmira Bengochea	pbengochea',
+// 'Sezen Asenova	sasenova',
+// 'José Luis Martínez	jmartínez',
+// 'Iván Ramirez	iramirez',
+// 'Ximena Páez	xpáez',
+// 'Estefanía Rosso	erosso',
+// 'Federico Abdo	fabdo',
+// 'Santiago de los santos	ssantos',
+// 'Analia Scapino	ascapino',
+// 'Valentina Palladino	vpalladino',
+// 'Alejo Gestal	agestal',
+// 'Marcos Brites	mbrites',
+// 'Florencia Gonnet	fgonnet',
+// 'Lucia Olivera	lolivera',
+// 'Macarena Fernández	mfernández',
+// 'Florencia Ferreira	fferreira',
+// 'Gastón Aboal	gaboal',
+// 'Fabricio Fernández	ffernández',
+// 'Mario Nicolini	mnicolini',
+// 'Abril Perdomo	aperdomo',
+// 'Mariela Chocho	mchocho',
+// 'Lucio Vera	lvera',
+// 'Mauro Yiansens	myiansens',
+// 'Valeria Bernasconi	vbernasconi',
+// 'Silvina Angüilla	sangüilla',
+// 'Juan Gabriel Garcia	jgarcia',
+// 'Marcos Zinnato	mzinnato',
+// 'Gonzalo Esposto	gesposto',
+// 'Mariana Neves	mneves',
+// 'Matilde acosta	macosta',
+// 'Virginia Ormazabal	vormazabal',
+// 'Emanuel Sosa	esosa',
+// 'Cristian Díaz	cdíaz',
+// 'Bruno Silvestri	bsilvestri',
+// 'Gonzalo Paredes	gparedes',
+// 'Matias Ortiz	mortiz',
+// 'German Gimenez	ggimenez',
+// 'Damian Clavell	dclavell',
+// 'Laura Conti	lconti',
+// 'Valentina	vvalentina',
+// 'Carmela Machado	cmachado',
+// 'Selva Gelso	sgelso',
+// 'Jessica Rosano	jrosano',
+// 'Valentina Ramirez	vramirez',
+// 'Joaquín Pereira	jpereira',
+// 'Cris Castro	ccastro',
+// 'Ismael González	igonzález',
+// 'Santiago Miguel	smiguel',
+// 'Germán Arén	garén',
+// 'Noha Camaño	ncamaño',
+// 'Laura Cabrera	lcabrera',
+// 'Julieta Sosa	jsosa',
+// 'Ana Duce	aduce',
+// 'Lucia Frizzi	lfrizzi',
+// 'Stefanie Vázquez	svázquez',
+// 'Nicolas Bichinque	nbichinque',
+// 'Esmeralda Pieri	epieri',
+// 'Antonella Díaz	adíaz',
+// 'Chiara inzaurralde	cinzaurralde',
+// 'Nicolás Viera	nviera',
+// 'Joaquina Goicoechea	jgoicoechea',
+// 'Romina Dominzain	rdominzain',
+// 'diego rosati	drosati',
+// 'Roman Ferreira	rferreira',
+// 'Rodrigo Silva	rsilva',
+// 'Cristian Marrero	cmarrero',
+// 'Moriana Gimenez	mgimenez',
+// 'Diandra de Aguiar	daguiar',
+// 'Alicia Martínez	amartínez',
+// 'Juan David	jdavid',
+// 'Carolina Antúnez	cantúnez',
+// 'Sofia Dri	sdri',
+// 'Cecilia Buonomo	cbuonomo',
+// 'Sofía ferreira	sferreira',
+// 'Victoria Goyret	vgoyret',
+// 'Michael Pla	mpla',
+// 'Andrea Carbonell	acarbonell',
+// 'José Abreu	jabreu',
+// 'Helian Gómez	hgómez',
+// 'Joshua Cardozo	jcardozo',
+// 'Miriam Gonzalez	mgonzalez',
+// 'Mauro Navarrine	mnavarrine',
+// 'Matilde Leites	mleites',
+// 'Pablo Pereyra	ppereyra',
+// 'Ivana Toribio	itoribio',
+// 'Allison Leoni	aleoni',
+// 'Josefa Flores	jflores',
+// 'Rocío Lión	rlión',
+// 'Natalia Rodríguez	nrodríguez',
+// 'Marinele De paula	mpaula',
+// 'Germán Olazabal	golazabal',
+// 'Martina Sosa	msosa',
+// 'Sofía Draper	sdraper',
+// 'Virginia Pérez	vpérez',
+// 'Matías Méndez	mméndez',
+// 'Nicolás Suárez	nsuárez',
+// 'Ana Bentancor	abentancor',
+// 'Agustin Hu	ahu',
+// 'Clara Gómez	cgómez',
+// 'NATALIA VARELA	nvarela',
+// 'Marcel Barceló	mbarceló',
+// 'Federico Gomez	fgomez',
+// 'Adriana Longo	alongo',
+// 'Francisco Pieri	fpieri',
+// 'Carina Pacheco	cpacheco',
+// 'Mariana pouso	mpouso',
+// 'Milena Cantero	mcantero',
+// 'Luciana Britos	lbritos',
+// 'Victoria sosa	vsosa',
+// 'Daniela Malleville	dmalleville',
+// 'Cecilia Barate	cbarate',
+// 'Sabrina Elgart	selgart',
+// 'Nico Tolstoy	ntolstoy',
+// 'Sheila Crida	scrida',
+// 'Bruno Dho	bdho',
+// 'Juana de los santos	jsantos',
+// 'Regignaldo Gonzalez	rgonzalez',
+// 'Claudia Rodríguez	crodríguez',
+// 'Karina Casavieja	kcasavieja',
+// 'Daniela González	dgonzález',
+// 'Andrea Dominzain	adominzain',
+// 'Augusto Deganello	adeganello',
+// 'Joaquín Sanabria	jsanabria',
+// 'Natalia Chiesa	nchiesa',
+// 'Camila Riesgo	criesgo',
+// 'Federico Guridi	fguridi',
+// 'Andres Delfino	adelfino',
+// 'Pablo ferragut	pferragut',
+// 'Priscilla Suarez	psuarez',
+// 'Dana Silva	dsilva',
+// 'Mercedes Méndez	mméndez',
+// 'Fabian Leoni	fleoni',
+// 'Andres Saavedra	asaavedra',
+// 'Mauricio Chirino	mchirino',
+// 'Gonzalo Vallarino	gvallarino',
+// 'Ramiro Hernandez	rhernandez',
+// 'Sandra Moreno	smoreno',
+// 'Romina Sosa	rsosa',
+// 'Cecilia Ogara	cogara',
+// 'Juan Pereyra	jpereyra',
+// 'Vanessa Astapenco	vastapenco',
+// 'Federico Mello	fmello',
+// 'Salomé Campot	scampot',
+// 'Sofía Caballero	scaballero',
+// 'Ruddy Moncada	rmoncada',
+// 'Ignacio Marquez	imarquez',
+// 'Antonio Manzi	amanzi',
+// 'Higinio Duarte	hduarte',
+// 'Natalia García	ngarcía',
+// 'Catalina Barceló	cbarceló',
+// 'Tomás Garula	tgarula',
+// 'Silvina Vilches	svilches',
+// 'Lia Lloveras	llloveras',
+// 'Joselin Damian	jdamian',
+// 'Marcelo Lozano	mlozano',
+// 'Luis Fernández	lfernández',
+// 'Guillermo Suárez	gsuárez',
+// 'Jorge Fuentes	jfuentes',
+// 'Federico Abdo	fabdo',
+// 'John Pirez	jpirez',
+// 'Marelen Cabrera	mcabrera',
+// 'Lucas guedes	lguedes',
+// 'Braian Texeira	btexeira',
+// 'Lucia Fornaroli	lfornaroli',
+// 'Luis Abdo	labdo',
+// 'Daniel Riesgo	driesgo',
+// 'Patricia Riesgo	priesgo',
+// 'Lois Ferreira	lferreira',
+// 'Amparo Bengochea	abengochea',
+// 'Elsa Romero	eromero',
+// 'María Sosa	msosa',
+// 'Nicolas Navascues	nnavascues',
+// 'Claudia Reyes	creyes',
+// 'josefina barceló	jbarceló',
+// 'Federico Lemos	flemos',
+// 'Martin Saavedra	msaavedra',
+// 'Andrés Kent	akent',
+// 'Pablo Pinazzo	ppinazzo',
+// 'Gonzalo Marmissolle	gmarmissolle',
+// 'Antonella Sosa	asosa',
+// 'Carolina Urioste	curioste',
+// 'Maximiliano Nogueira	mnogueira',
+// 'Emilse Chá	echá',
+// 'Lucas Fuente	lfuente',
+// 'Valentín Pérez	vpérez',
+// 'Francisco González	fgonzález',
+// 'Magdalena Risvegliato	mrisvegliato',
+// 'Santiago Sánchez	ssánchez',
+// 'Diego Fuentes	dfuentes',
+// 'Facundo Andrioli	fandrioli',
+// 'Martín Riesgo	mriesgo',
+// 'Fernando Mangold	fmangold',
+// 'Victor Estigarribia	vestigarribia',
+// 'Jessica Paez	jpaez',
+// 'Clara Gonzalez	cgonzalez',
+// 'Lorena Guardia	lguardia',
+// 'Marco Bonino	mbonino',
+// 'Federico Prieto	fprieto',
+// 'Nahuel Granero	ngranero',
+// 'Nicolás Zinoveev	nzinoveev',
+// 'Ricardo pla	rpla',
+// 'Emilia Diaz	ediaz',
+// 'Paula Galíndez	pgalíndez',
+// 'Lucía Prieto	lprieto',
+// 'Paula Pouso	ppouso',
+// 'Genaro Loprete	gloprete',
+// 'Andrea Olariaga	aolariaga',
+// 'Luis Chiappara	lchiappara',
+// 'Agustín ibero	aibero',
+// 'Gabriel Ducer	gducer',
+// 'Hebenor Bermúdez	hbermúdez',
+// 'Franco Alvarez	falvarez',
+// 'Óscar Gutiérrez	ógutiérrez',
+// 'Lorena Fleitas	lfleitas',
+// 'Franco Gargaglioni	fgargaglioni',
+// 'Joaquin Silva	jsilva',
+// 'Fernando Martínez	fmartínez',
+// 'Alejandro González	agonzález',
+// 'Guillermo González	ggonzález',
+// 'Verónica Pinazzo	vpinazzo',
+// 'Nahuel Quiroga	nquiroga',
+// 'Rodrigo Correa	rcorrea',
+// 'Emanuel Berón	eberón',
+// 'Emanuel Barrera	ebarrera',
+// 'Fabio Gatti	fgatti',
+// 'Rodrigo Sánchez	rsánchez',
+// 'Rodrigo Marote	rmarote',
+// 'Pablo Hernández	phernández',
+// 'Nicolás Revello	nrevello',
+// 'Máximo Saavedra	msaavedra',
+// 'Marta Borelli	mborelli',
+// 'Marcos Zinatto	mzinatto',
+// 'Jonathan Sjostrand	jsjostrand',
+// 'Gustavo Ricci	gricci',
+// 'Gissel Martins gmartins',
+// 'Felipe Hein	fhein',
+// 'Enzo de Nicola enicola',
+// 'Emil Chichet	echichet',
+// 'Diego Pilosof	dpilosof',
+// 'Darío García	dgarcía',
+// 'Avril Pardo	apardo',
+// 'Agustín Rodríguez	arodríguez',
+// 'Agustín Da Silveira	asilveira',
+// 'Fernando Abdo	fabdoc',
+// ];"
