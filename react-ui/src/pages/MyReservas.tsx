0d3e3533f42eefe746bd68b681680ed94aec80db
FrancoGargaglioni <FrancoGargaglioni@gmail.com>
2026-05-31 17:46:50 -0300

    style: revert inner border, blur, and shadow on list items to let them blend with the background

--- react-ui/src/pages/MyReservas.tsx
+++ react-ui/src/pages/MyReservas.tsx
@@ -336,14 +336,11 @@ export default function MyReservas() {
                 <div
                   key={reserva.hora}
                   style={{
-                    backgroundColor: isUserReserved ? '#07ce3250' : 'rgba(255,255,255,0.02)',
+                    backgroundColor: isUserReserved ? '#07ce3250' : 'transparent',
                     padding: '8px', // keep constant to avoid jump
-                    borderRadius: '8px',
-                    border: isUserReserved ? '2px solid #254e01ff' : '1px solid rgba(255,255,255,0.08)',
+                    borderRadius: '4px',
+                    border: isUserReserved ? '2px solid #254e01ff' : '2px solid transparent',
                     width: '100%',
-                    backdropFilter: 'blur(12px)',
-                    boxShadow: '0 4px 16px rgba(0,0,0,0.3)',
-                    marginBottom: '10px'
                   }}
                 >
                   {/* GRID ROW */}
